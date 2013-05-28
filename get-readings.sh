82cd8b1553d29437a61cac8b9d21ec6cbbc2cd79
James Deikun <james@place.org>
2013-05-28 10:49:50 -0400

    Improved behaviour of get-readings.sh on newly checked-out repos, also posix compliance.

--- get-readings.sh
+++ get-readings.sh
@@ -9,6 +9,18 @@ cd readings
 
 updated=no
 
+if git rev-parse --verify -q git-annex >/dev/null; then
+  :
+  # git-annex metadata already set up
+elif which git-annex >/dev/null; then
+  # get git-annex to do the setup itself, safest
+  git-annex init
+else
+  # make sure the git-annex branch is getting pulled
+  # XXX i hope you called upstream 'origin' -- how can i autosense this?
+  git branch --track git-annex origin/git-annex
+fi
+
 for i in *; do
   link="$(git cat-file blob :readings/"$i")"
   if [ -f "$link" ]; then
@@ -17,7 +29,7 @@ for i in *; do
   else
     key="$(basename -- "$link")"
     hash="$(echo -n "$key" | md5sum)"
-    url="$(git cat-file blob "git-annex:${hash:0:3}/${hash:3:3}/$key.log.web" | grep '^[^ ]\+ 1 ' | tail -n 1 | sed -e 's/^[^ ]\+ 1 //')"
+    url="$(git cat-file blob "git-annex:$(echo "$hash" | cut -c -3)/$(echo "$hash" | cut -c 4-6)/$key.log.web" | grep '^[^ ]\+ 1 ' | tail -n 1 | sed -e 's/^[^ ]\+ 1 //')"
     mkdir -p ../.git/annex/tmp
     tpath="../.git/annex/tmp/$key"
     if curl -fgL -o "$tpath" "$url"; then
@@ -38,7 +50,7 @@ for i in *; do
   fi
 done
 
-if [ "$updated" == yes ]; then
+if [ "$updated" = yes ]; then
   if which git-annex >/dev/null; then
     git-annex fsck
   fi
